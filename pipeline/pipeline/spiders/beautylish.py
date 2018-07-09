ddd21760bcc117073fc41e46b8d0dc6aa0fcc211
Ouahib El Hanchi <ouahib.el.hanchi@gmail.com>
2018-07-09 00:18:33 +0000

    update beautylish spider

--- pipeline/pipeline/spiders/beautylish.py
+++ pipeline/pipeline/spiders/beautylish.py
@@ -30,24 +30,22 @@ class BeautylishProductsSpider(scrapy.Spider):
         products = response.xpath('//a[@class="tile"]')
         for product in products:
             href = product.xpath('@href').extract_first()
-            url = response.urljoin(href)
-            yield scrapy.Request(url, callback=self.parse_product)
-            
+            yield response.follow(href, callback=self.parse_product)
+
         # Follow next page if it exists
         next_page = response.xpath('//span[@class="pager_next"]/a')
         if next_page:
             href = next_page.xpath('@href').extract_first()
-            url = response.urljoin(href)
-            yield scrapy.Request(url)
-        
+            yield response.follow(href)
+
     # -------------------------------------------------------------------------
-    
+
     def parse_product(self, response):
         """Extract product details"""
         encoded = response.xpath('//script').re_first('window.scriptCtx = "([^"]+)"')
         decoded = base64.b64decode(encoded)
         data = json.loads(decoded)
-        
+
         product_loader = ProductItemLoader(ProductItem(), response)
         product_loader.add_xpath('gtin', '//span[@itemprop="gtin13"]')
         product_loader.add_xpath('name', '//h1[@itemprop="name"]')
@@ -67,14 +65,14 @@ class BeautylishProductsSpider(scrapy.Spider):
         product_loader.add_value('url', response.url)
         product = product_loader.load_item()
         product['reviews'] = []
-        
+
         # Extract reviews if any, otherwise yield collected data
-        if product['reviewCount'] > 0:
+        if product['reviewCount']:
             cipherid = data['ProductApp']['product']['cipheredId']
             url = self.build_review_url(cipherid)
             yield scrapy.Request(
-                url, 
-                callback=self.parse_reviews, 
+                url,
+                callback=self.parse_reviews,
                 meta={
                     'product': product,
                     'cipherid': cipherid,
@@ -82,7 +80,7 @@ class BeautylishProductsSpider(scrapy.Spider):
             )
         else:
             yield product
-        
+
     # -------------------------------------------------------------------------
 
     def parse_reviews(self, response):
@@ -90,7 +88,7 @@ class BeautylishProductsSpider(scrapy.Spider):
         product = response.meta['product']
         cipherid = response.meta['cipherid']
         reviews = json.loads(response.body)
-        if len(reviews) > 0:
+        if reviews:
             for each in reviews:
                 # Extract review information
                 review_loader = ReviewItemLoader(ReviewItem())
@@ -98,25 +96,25 @@ class BeautylishProductsSpider(scrapy.Spider):
                 review_loader.add_value('description', each['text'])
                 review_loader.add_value('rating', each['rating'])
                 review_loader.add_value('helpfulCount', each['likesCount'])
-                review_loader.add_value('reviewImage', each['images'][0]['clUrl'] if len(each['images']) > 0 else None)
+                review_loader.add_value('reviewImage', each['images'][0]['clUrl'] if each['images'] else None)
                 review_loader.add_value('datePublished', each['isoDate'])
                 review = review_loader.load_item()
-                
+
                 # Extract reviewer information
                 reviewer_loader = ReviewerItemLoader(ReviewerItem())
                 reviewer_loader.add_value('name', each['userDisplayName'])
                 reviewer_loader.add_value('profileUrl', each['userUrl'])
                 reviewer = reviewer_loader.load_item()
-                
+
                 review['reviewer'] = reviewer
                 product['reviews'].append(review)
-                
+
             limit = 20
             offset = response.meta.get('offset', 0) + limit
             url = self.build_review_url(cipherid, offset)
             yield scrapy.Request(
-                url, 
-                callback=self.parse_reviews, 
+                url,
+                callback=self.parse_reviews,
                 meta={
                     'product': product,
                     'cipherid': cipherid,
@@ -126,9 +124,9 @@ class BeautylishProductsSpider(scrapy.Spider):
         else:
             # No more reviews, yield the collected data
             yield product
-        
+
     # -------------------------------------------------------------------------
-    
+
     def build_review_url(self, cipherid, offset=0, limit=20):
         """Build review url from cipherid"""
         base_url = "https://www.beautylish.com/rest/reviews/p-{cipherid}".format(cipherid=cipherid)
