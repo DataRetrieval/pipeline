2897da45ea022ac9b5957cd6788832b7479492a8
Ouahib El Hanchi <ouahib.el.hanchi@gmail.com>
2018-07-23 17:12:09 +0000

    update sephora

--- pipeline/pipeline/spiders/sephora.py
+++ pipeline/pipeline/spiders/sephora.py
@@ -27,38 +27,41 @@ class SephoraProductsSpider(SitemapSpider):
 
     def parse(self, response):
         """Extract product details"""
-        data = response.xpath('//script[@seph-json-to-js="sku"]/text()').extract_first()
-        if data:
-            record = json.loads(data)
-            product_loader = ProductItemLoader(ProductItem(), response)
-            product_loader.add_value('id', record['id'])
-            product_loader.add_value('sku', record['sku_number'])
-            product_loader.add_value('name', record['primary_product']['display_name'])
-            product_loader.add_value('brand', record['primary_product']['brand_name'])
-            product_loader.add_xpath('category', '//ol[@data-at="nav_crumb"]/li')
-            product_loader.add_xpath('loveCount', '//span[@seph-love-count]/@seph-love-count')
-            product_loader.add_xpath('reviewCount', '//a[@href="#pdp-reviews"]/span[@class="u-linkComplexTarget"]', re='([0-9]+)')
-            product_loader.add_value('rating', record['primary_product']['rating'])
-            product_loader.add_value('listPrice', record.get('list_price'))
-            product_loader.add_value('valuePrice', record.get('value_price'))
-            product_loader.add_value('size', record['sku_size'])
-            product_loader.add_xpath('image', '//meta[@property="og:image"]/@content')
-            product_loader.add_xpath('use', '//div[@id="use"]')
-            product_loader.add_xpath('aboutBrand', '//div[@id="brand"]')
-            product_loader.add_xpath('details', '//div[@id="details"]')
-            product_loader.add_xpath('ingredients', '//div[@id="ingredients"]/p[not(@class="ng-hide")]')
-            product_loader.add_value('ingredients', record['ingredients'])
-            product_loader.add_xpath('shipping', '//div[@id="shipping"]/p[not(@class="ng-hide")]')
-            product_loader.add_value('sephoraExclusive', record.get('is_sephora_exclusive', False))
-            product_loader.add_value('url', response.url)
-            product = product_loader.load_item()
+        data = response.xpath(
+            '//script[@seph-json-to-js="sku"]/text()').extract_first()
+        if not data:
+            return None
 
-            reviews_url = response.xpath('//iframe[contains(@src, "reviews.htm")]/@src').re_first('([^?]+)')
-            return scrapy.Request(
-                '{url}?format=embedded'.format(url=reviews_url),
-                callback=self.parse_reviews,
-                meta={'product': product}
-            )
+        record = json.loads(data)
+        product_loader = ProductItemLoader(ProductItem(), response)
+        product_loader.add_value('id', record['id'])
+        product_loader.add_value('sku', record['sku_number'])
+        product_loader.add_value('name', record['primary_product']['display_name'])
+        product_loader.add_value('brand', record['primary_product']['brand_name'])
+        product_loader.add_xpath('category', '//ol[@data-at="nav_crumb"]/li')
+        product_loader.add_xpath('loveCount', '//span[@seph-love-count]/@seph-love-count')
+        product_loader.add_xpath('reviewCount', '//a[@href="#pdp-reviews"]/span[@class="u-linkComplexTarget"]', re='([0-9]+)')
+        product_loader.add_value('rating', record['primary_product']['rating'])
+        product_loader.add_value('listPrice', record.get('list_price'))
+        product_loader.add_value('valuePrice', record.get('value_price'))
+        product_loader.add_value('size', record['sku_size'])
+        product_loader.add_xpath('image', '//meta[@property="og:image"]/@content')
+        product_loader.add_xpath('use', '//div[@id="use"]')
+        product_loader.add_xpath('aboutBrand', '//div[@id="brand"]')
+        product_loader.add_xpath('details', '//div[@id="details"]')
+        product_loader.add_xpath('ingredients', '//div[@id="ingredients"]/p[not(@class="ng-hide")]')
+        product_loader.add_value('ingredients', record['ingredients'])
+        product_loader.add_xpath('shipping', '//div[@id="shipping"]/p[not(@class="ng-hide")]')
+        product_loader.add_value('sephoraExclusive', record.get('is_sephora_exclusive', False))
+        product_loader.add_value('url', response.url)
+        product = product_loader.load_item()
+
+        reviews_url = response.xpath('//iframe[contains(@src, "reviews.htm")]/@src').re_first('([^?]+)')
+        return scrapy.Request(
+            '{url}?format=embedded'.format(url=reviews_url),
+            callback=self.parse_reviews,
+            meta={'product': product}
+        )
 
     # -------------------------------------------------------------------------
 
